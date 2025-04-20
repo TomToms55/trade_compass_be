e9cffadf8ed98df23b02b24a6c08cf799578232f
Tomás <tomassoares__@hotmail.com>
2025-04-20 18:02:50 +0100

    deploy fix

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 
 # Simple deploy script for MVP
 ssh -i /c/Users/tomas/.ssh/id_rsa root@139.180.201.180 << 'ENDSSH'
-  cd /trade-compass-be
-  git pull origin main
+  cd ./trade_compass_be
+  git pull origin master
   npm install
   npm run build
-  pm2 start dist/index.js --name trade-compass-be --time
+  pm2 restart trade-compass-be --time -f || pm2 start ./dist/index.js --name trade-compass-be --time -f
 ENDSSH
